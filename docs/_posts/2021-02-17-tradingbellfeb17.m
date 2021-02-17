ec6007098ed738a120a2e9421e65cf260cdf8777
Kurnutalas <64170552+Kurnutalas@users.noreply.github.com>
2021-02-17 08:44:00 +0530

    Create 2021-02-17-tradingbellfeb17.m

---
+++ docs/_posts/2021-02-17-tradingbellfeb17.m
@@ -0,0 +1,28 @@
+# Points you need to know before the Trading bell
+1. NIFTY futures is trading below 15300 levels. Dow Jones closed above 31500 and futures trading flat near 31450.
+2. WTI Crude and Brent Crude December futures at 59.88 & 62.69 $ respectively. 
+3. Key of levels of Nifty: we expect Nifty to open negative below 15300 levels, key supports at 15226 and 15139, key resistances at 15416 and 15519.
+4. Key levels of bank nifty: bank nifty key supports at 36671 and 36243, key resistances at 37617 and 38136.
+5. Most number of call options at 16000 strike and most number of put options at 15000 strike.
+6. USD at 72.69 INR, nil change
+7. Global triggers:Japan Trade balance, UK CPI, UK PPI input, ECB Monetary policy statement, US PPI, US Retail Sales, US Industrial Production, US Business Inventories, US Retail Inventories Ex Auto, US FOMC meeting minutes, US API weekly Crude oil stock
+8. Results today: Pasupati Spinning, Sanghvi Forging and Engineering, SR Industries, Uniply Decor and Uniply Industries
+
+NIFTY to open gap down below 15300 levels. NIFTY shall be opening below the pivot i.e. 15329.
+
+Stocks to watch:
+1. Adani Ports earmarked Rs 10,000 crore to build a new gateway into Maharashtra, and acquired Dighi Ports for Rs 705 crore.
+2. TVS Srichakra signed a Memorandum of Understanding with the Tamil Nadu government to facilitate the investment programme of Rs 1,000 crore.
+3. L&T Finance Holdings closed a rights issue of Rs 2,998.61 crore, which was oversubscribed by 15 percent.
+4. Nestle India reported higher profit at Rs 483.3 crore in Q4CY20 against Rs 472.6 crore Q4CY19; revenue rose to Rs 3,432.6 crore from Rs 3,149.3 crore YoY.
+5. Heritage Novandie Foods, a 50:50 joint venture company between Heritage Foods and Novandie, France started commercial production.
+6. Infosys, the US subsidiary completed acquisition of automated new business and underwriting platform from STEP Solutions.
+7. ICRA upgraded the Bajaj Electricals's long-term credit rating to A from A-, with stable outlook.
+8. East West Holdings will consider bonus issue on February 19.
+9. Ultratech board approves fundraising of $400 million
+10. CLSA maintains buy on Axis, ICICI, SBI banks and Max Fin Services
+11. UBS gives buy on JSW energy with target of 87
+12. Emkay gives buy on Varun Beverages with a target of 1000+
+13. IndusInd bank plans fundraising
+14. India Ratings gives Vedanta outlook to stable from negative
+15. INOX wind bags 62 MW wind project orders
